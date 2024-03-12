24a7ec1201d59c1c3dcd5989adb8e212e6fef797
Marie-Pierre Oudot <marie-pierre.oudot@cea.fr>
2024-03-12 11:45:08 +0100

    Correct test for Issue#72 not to call python subprocess (doesn't work on CI container)

--- test_link/test_issue72.py
+++ test_link/test_issue72.py
@@ -1,20 +1,11 @@
 import sys
 import pyMagix3D as Mgx3D
-import subprocess
-
-SEGFAULT_PROCESS_RETURNCODE = -11
 
 def test_issue72():
     ctx = Mgx3D.getStdContext()
-    ctx.clearSession() # Clean the session after the previous test
-    try:
-        subprocess.run(["python3", "-m", "issue72"], check=True)
-    except subprocess.CalledProcessError as err:
-        if err.returncode == SEGFAULT_PROCESS_RETURNCODE:
-            print("probably segfaulted")
-            assert False
-        else:
-            print(f"crashed for other reasons: {err.returncode}")
-            assert False
-    else:
-            assert True
+    #  Création d'une boite avec une topologie
+    ctx.getTopoManager().newBoxWithTopo (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), 10, 10, 10)
+    #  Découpage en O-grid des blocs structurés Bl0000
+    ctx.getTopoManager().splitBlocksWithOgridV2 (["Bl0000"], [], .5, 10)
+    # Fusion de sommets Som0014 avec Som006
+    ctx.getTopoManager().snapVertices("Som0014", "Som0006", True)
