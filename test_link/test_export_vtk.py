b901daf75d8a701236d565ffb216b888fe89b111
Benoit Lelandais <41573600+lelandaisb@users.noreply.github.com>
2024-04-25 18:37:22 +0200

    Update test_export_vtk.py

--- test_link/test_export_vtk.py
+++ test_link/test_export_vtk.py
@@ -4,6 +4,8 @@ import pyMagix3D as Mgx3D
 vtkfilename = "box.vtk"
 
 def test_export_box():
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
     # Création de la boite Vol0000
     ctx.getGeomManager().newBox (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1))
     # Création d'un bloc topologique non structuré sur une géométrie (Vol0000)
