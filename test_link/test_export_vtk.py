2ea04b9f58f6141d04cacb651a4435fa85632957
Marie-Pierre Oudot <marie-pierre.oudot@cea.fr>
2024-04-25 09:30:24 +0200

    Add test for VTK export

---
+++ test_link/test_export_vtk.py
@@ -0,0 +1,15 @@
+import sys
+import pyMagix3D as Mgx3D
+
+vtkfilename = "box.vtk"
+
+def test_export_box():
+    # Création de la boite Vol0000
+    ctx.getGeomManager().newBox (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1))
+    # Création d'un bloc topologique non structuré sur une géométrie (Vol0000)
+    ctx.getTopoManager().newUnstructuredTopoOnGeometry ("Vol0000")
+    # Création du maillage pour toutes les faces
+    ctx.getMeshManager().newAllFacesMesh()
+    ctx.getGeomManager().exportVTK(vtkfilename)
+    assert os.path.exists(vtkfilename)
+    assert os.path.getsize(vtkfilename) > 0
