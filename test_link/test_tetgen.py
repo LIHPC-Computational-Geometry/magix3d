62cbd04bfd295dd17370cdbd18f470d4d1b181d0
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2024-06-25 14:23:40 +0200

    New unit test for tetgen

---
+++ test_link/test_tetgen.py
@@ -0,0 +1,10 @@
+import sys
+import pyMagix3D as Mgx3D
+
+def test_tetgen():
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+    # Création d'une boite avec une topologie
+    ctx.getTopoManager().newBoxWithTopo(Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), False)
+    # Création du maillage pour tous les blocs
+    ctx.getMeshManager().newAllBlocksMesh()
