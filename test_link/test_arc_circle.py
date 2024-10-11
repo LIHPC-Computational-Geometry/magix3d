909e892df89a7af7c210620a727118803952ba8c
calderans <calderan.simon@gmail.com>
2024-10-11 16:04:54 +0200

    - Changed from Utils::Point to GeomVertex in test_arc_circle.py

--- test_link/test_arc_circle.py
+++ test_link/test_arc_circle.py
@@ -4,7 +4,10 @@ def test_circle_by_center():
     ctx = Mgx3D.getStdContext()
     ctx.clearSession() # Clean the session after the previous test
     gm = ctx.getGeomManager ()
-    gm.newArcCircle(Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 0, 0), Mgx3D.Point(2, 0, 0), True)
+    p0 = gm.newVertex (Mgx3D.Point(0, 0, 0))
+    p1 = gm.newVertex (Mgx3D.Point(1, 0, 0))
+    p2 = gm.newVertex (Mgx3D.Point(2, 0, 0))
+    gm.newArcCircle(p0, p1, p2, True)
 
     assert gm.getNbCurves()==1
 
@@ -12,7 +15,14 @@ def test_circle_by_3points():
     ctx = Mgx3D.getStdContext()
     ctx.clearSession() # Clean the session after the previous test
     gm = ctx.getGeomManager ()
-    gm.newArcCircle(Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 0, 0), Mgx3D.Point(1, 1, 0))
-    gm.newArcCircle(Mgx3D.Point(0, 0, 1), Mgx3D.Point(1, 0, 1), Mgx3D.Point(1, 1, 1),"aaa")
+    p0 = gm.newVertex (Mgx3D.Point(0, 0, 0))
+    p1 = gm.newVertex (Mgx3D.Point(1, 0, 0))
+    p2 = gm.newVertex (Mgx3D.Point(1, 1, 0))
+    gm.newArcCircle(p0, p1, p2)
+
+    p3 = gm.newVertex (Mgx3D.Point(0, 0, 1))
+    p4 = gm.newVertex (Mgx3D.Point(1, 0, 1))
+    p5 = gm.newVertex (Mgx3D.Point(1, 1, 1))
+    gm.newArcCircle(p3, p4, p5,"aaa")
 
     assert gm.getNbCurves()==2
