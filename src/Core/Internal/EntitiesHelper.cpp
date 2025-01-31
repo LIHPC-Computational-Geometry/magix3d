b701336556332bfa7ac6f78f9abbc3912da1dbbe
nicolas le goff <14815625+nicolaslg@users.noreply.github.com>
2025-01-31 13:10:29 +0100

    Update src/Core/Internal/EntitiesHelper.cpp
    replaced DBL_MAX and MIN with std::numeric_limits

--- src/Core/Internal/EntitiesHelper.cpp
+++ src/Core/Internal/EntitiesHelper.cpp
@@ -667,8 +667,8 @@ double EntitiesHelper::angle(const Utils::Entity* cec, const Utils::Entity* ce1,
 /*----------------------------------------------------------------------------*/
 bool EntitiesHelper::getBounds (const std::vector<Utils::Entity*>& entities, double bounds [6])
 {
-	bounds [0]	= bounds [2]	= bounds [4]	= DBL_MAX;
-	bounds [1]	= bounds [3]	= bounds [5]	= -DBL_MAX;
+	bounds [0]	= bounds [2]	= bounds [4]	= std::numeric_limits<double>::max();
+	bounds [1]	= bounds [3]	= bounds [5]	= std::numeric_limits<double>::min();
 	if (true == entities.empty ( ))
 		return false;
 
