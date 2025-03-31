c7becbbefff15feac10c6d6d57e3874d840e5f02
nicolas le goff <nicolas.le-goff@cea.fr>
2025-03-31 09:30:30 +0200

    add atest that fails when executing one sectionbyplane command on two volumes

--- test_link/test_sectionbyplane.py
+++ test_link/test_sectionbyplane.py
@@ -1,6 +1,6 @@
 import pyMagix3D as Mgx3D
 
-def test_sectionbyplane():
+def test_sectionbyplane_0():
     ctx = Mgx3D.getStdContext()
     ctx.clearSession() # Clean the session after the previous test
     tm = ctx.getTopoManager ()
@@ -13,5 +13,28 @@ def test_sectionbyplane():
     gm.sectionByPlane (["Vol0002"], Mgx3D.Plane(Mgx3D.Point(0, 0, .2), Mgx3D.Vector(0, 0, 1)), "")
     nbpoints = gm.getNbVertices()
     nbcurves = gm.getNbCurves()
+    nbsurfaces = gm.getNbSurfaces()
+    nbvolumes = gm.getNbVolumes()
     assert nbpoints == 24
     assert nbcurves == 44
+    assert nbsurfaces == 24
+    assert nbvolumes == 5
+
+def test_sectionbyplane_1():
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+    tm = ctx.getTopoManager ()
+    gm = ctx.getGeomManager ()
+    gm.newBox(Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1))
+    gm.newBox(Mgx3D.Point(1, 0, 0), Mgx3D.Point(1.5, 1, 1))
+    gm.newBox(Mgx3D.Point(1.5, 0, 0), Mgx3D.Point(3, 1, 1))
+    gm.glue (["Vol0000","Vol0001","Vol0002"])
+    gm.sectionByPlane (["Vol0000", "Vol0002"], Mgx3D.Plane(Mgx3D.Point(0, 0, .2), Mgx3D.Vector(0, 0, 1)), "")
+    nbpoints = gm.getNbVertices()
+    nbcurves = gm.getNbCurves()
+    nbsurfaces = gm.getNbSurfaces()
+    nbvolumes = gm.getNbVolumes()
+    assert nbpoints == 24
+    assert nbcurves == 44
+    assert nbsurfaces == 24
+    assert nbvolumes == 5
