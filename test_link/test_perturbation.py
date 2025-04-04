6bbb3385c87a0ee8b124b03814d8a72ee5c0cd4c
Marie-Pierre Oudot <marie-pierre.oudot@cea.fr>
2025-04-04 15:42:52 +0200

    Tests refactoring

--- test_link/test_perturbation.py
+++ test_link/test_perturbation.py
@@ -1,10 +1,9 @@
 import pyMagix3D as Mgx3D
 import pytest
 
-def test_perturbation(capfd):
+def test_perturbation1(capfd):
     ctx = Mgx3D.getStdContext()
     ctx.clearSession() # Clean the session after the previous test
-
     def pert(x,y,z):
         if(z>=0.4):
             z = 0.4
@@ -17,7 +16,7 @@ def test_perturbation(capfd):
     out, err = capfd.readouterr()
     assert("Des hexaédres semblent inversés dans le maillage structuré" not in err)
 
-def test_perturbation_surf(capfd):
+def test_perturbation2(capfd):
     ctx = Mgx3D.getStdContext()
     ctx.clearSession() # Clean the session after the previous test
     def pert(x,y,z):
@@ -40,3 +39,22 @@ def test_perturbation_surf(capfd):
     ctx.getMeshManager().newFacesMesh ( ["Fa0014"] )
     out, err = capfd.readouterr()
     assert("Des polygones semblent retournés" not in out)
+
+def test_perturbation3(capfd):
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+    def pert(x, y, z):
+        return [x, y, z+x/10.0 + y/5.0]
+
+    # Création d'une boite avec une topologie
+    ctx.getTopoManager().newBoxWithTopo (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), 10, 10, 10)
+    # Modifie le groupe ZMAX
+    ctx.getGeomManager().addToGroup (["Surf0005"], 2, "ZMAX")
+    #ctx.getGroupManager().addCartesianPerturbation("ZMAX", pert)
+    # Création du maillage pour des faces
+    ctx.getMeshManager().newFacesMesh ( ["Fa0005"] )
+    # Sauvegarde du maillage (mli)
+    ctx.getMeshManager().writeMli("perturbation3.mli2")
+
+    out, err = capfd.readouterr()
+    assert len(err) == 0
