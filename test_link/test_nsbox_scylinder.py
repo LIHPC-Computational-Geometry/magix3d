aba345307ebc2623aa0561610cf1c8a9f41e3568
Marie-Pierre Oudot <marie-pierre.oudot@cea.fr>
2025-04-04 15:42:52 +0200

    Tests refactoring

--- test_link/test_nsbox_scylinder.py
+++ test_link/test_nsbox_scylinder.py
@@ -1,7 +1,6 @@
 import pyMagix3D as Mgx3D
-import os
 
-def test_nsbox_scylinder_ok(capfd):
+def test_nsbox_scylinder(capfd):
     ctx = Mgx3D.getStdContext()
     gm = ctx.getGeomManager()
     tm = ctx.getTopoManager()
@@ -20,27 +19,4 @@ def test_nsbox_scylinder_ok(capfd):
 
     out, err = capfd.readouterr()
     assert len(err) == 0
-
-def test_nsbox_scylinder_error(capfd):
-    ctx = Mgx3D.getStdContext()
-    gm = ctx.getGeomManager()
-    tm = ctx.getTopoManager()
-
-    ctx.clearSession() # Clean the session after the previous test
-    # Création d'un cylindre
-    gm.newCylinder(Mgx3D.Point(0, 0, 0), .2, Mgx3D.Vector(1, 0, 0), Mgx3D.Portion.QUART)
-    # Création d'une boite
-    gm.newBox (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1))
-    # Rotation d'une géométrie
-    gm.rotate(["Vol0000"], Mgx3D.RotX(270))
-    # Collage entre géométries
-    gm.glue(["Vol0001", "Vol0000"])
-    try:
-        # Création d'un bloc topologique sur une géométrie => PB: ce n'est plus un cylindre après le glue
-        tm.newTopoOGridOnGeometry("Vol0000", 0.5)
-        assert False
-    except RuntimeError:
-        out, err = capfd.readouterr()
-        expected = "CommandNewTopoOGridOnGeometry impossible, entité Vol0000 n'est pas d'un type supporté pour le moment"
-        assert expected in err
 
