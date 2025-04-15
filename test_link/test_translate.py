f0937cb001a5ac1a0a3fee4a0bcb72a87abef604
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2025-04-15 14:41:17 +0200

    Redo on translate moved from unresolved to resolved

--- test_link/test_translate.py
+++ test_link/test_translate.py
@@ -1,5 +1,4 @@
 import pyMagix3D as Mgx3D
-import math
 
 def test_translate(capfd):
     ctx = Mgx3D.getStdContext()
@@ -31,6 +30,37 @@ def test_translate(capfd):
 
     out, err = capfd.readouterr()
     assert len(err) == 0
+
+def test_redo_translate():
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+    gm = ctx.getGeomManager()
+    tm = ctx.getTopoManager()
+
+    # Création de la boite Vol0000
+    gm.newBox (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1))
+    # Création de la boite Vol0001
+    gm.newBox (Mgx3D.Point(1, 0, 0), Mgx3D.Point(2, 1, 1))
+    # Collage entre Vol0000 Vol0001
+    gm.glue (["Vol0000","Vol0001"])
+    # Création d'un bloc topologique structuré sur une géométrie (Vol0000)
+    tm.newStructuredTopoOnGeometry ("Vol0000")
+    # Création d'un bloc topologique structuré sur une géométrie (Vol0001)
+    tm.newStructuredTopoOnGeometry ("Vol0001")
+
+    p1 = gm.getCoord("Pt0001")
+    s1 = tm.getCoord("Som0001")
+    assertPoints(gm, tm, 0, 0, 0)
+
+    # Translation d'une géométrie avec sa topologie
+    gm.translate (["Vol0000","Vol0001"], Mgx3D.Vector(1, 2, 3))
+    assertPoints(gm, tm, 1, 2, 3)
+
+    ctx.undo()
+    assertPoints(gm, tm, 0, 0, 0)
+
+    ctx.redo()
+    assertPoints(gm, tm, 1, 2, 3)
 
 def assertPoints(gm, tm, x, y, z):
     p1 = gm.getCoord("Pt0001")
