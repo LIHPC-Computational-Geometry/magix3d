85d96e15e28a2d02dfa3f01800ab031d7592c7e3
Marie-Pierre Oudot <marie-pierre.oudot@cea.fr>
2025-04-24 15:35:14 +0200

    New tests
    # Conflicts:
    #	test_link/test_projection.py

--- test_link/test_export_vtk.py
+++ test_link/test_export_vtk.py
@@ -1,17 +1,55 @@
 import os
 import pyMagix3D as Mgx3D
 
-vtkfilename = "box.vtk"
 
-def test_export_box():
+def test_export_box(capfd):
     ctx = Mgx3D.getStdContext()
     ctx.clearSession() # Clean the session after the previous test
-    # Création de la boite Vol0000
+
+    vtkfilename = "box.vtk"
     ctx.getGeomManager().newBox (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1))
-    # Création d'un bloc topologique non structuré sur une géométrie (Vol0000)
-    ctx.getTopoManager().newUnstructuredTopoOnGeometry ("Vol0000")
-    # Création du maillage pour toutes les faces
+    ctx.getTopoManager().newUnstructuredTopoOnGeometry("Vol0000")
     ctx.getMeshManager().newAllFacesMesh()
     ctx.getGeomManager().exportVTK(vtkfilename)
+
     assert os.path.exists(vtkfilename)
     assert os.path.getsize(vtkfilename) > 0
+    out, err = capfd.readouterr()
+    assert len(err) == 0
+
+def test_export_cylinder(capfd):
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+
+    vtkfilename = "cylinder.vtk"
+    ctx.getGeomManager().newCylinder(Mgx3D.Point(0.3, 0.5, 0.5), 0.4, Mgx3D.Vector(0.4, 0, 0), 360)
+    ctx.getTopoManager().newUnstructuredTopoOnGeometry("Vol0000")
+    ctx.getMeshManager().newAllFacesMesh()
+    ctx.getGeomManager().exportVTK(vtkfilename)
+
+    assert os.path.exists(vtkfilename)
+    assert os.path.getsize(vtkfilename) > 0
+    out, err = capfd.readouterr()
+    assert len(err) == 0
+
+def test_export_imported_step(capfd):
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+
+    test_folder = os.path.dirname(__file__)
+    step_file_path = os.path.join(test_folder, 'B0.step')
+    # Import STEP
+    # Changement d'unité de longueur
+    ctx.setLengthUnit(Mgx3D.Unit.meter)
+    # Import STEP
+    ctx.getGeomManager().importSTEP(step_file_path)
+
+    vtkfilename = "B0.vtk"
+    ctx.getTopoManager().newUnstructuredTopoOnGeometry("Vol0000")
+    ctx.getMeshManager().newAllFacesMesh()
+    ctx.getGeomManager().exportVTK(vtkfilename)
+
+    assert os.path.exists(vtkfilename)
+    assert os.path.getsize(vtkfilename) > 0
+    out, err = capfd.readouterr()
+    assert len(err) == 0
