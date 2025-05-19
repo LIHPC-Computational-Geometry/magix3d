b1ae92da3f9446a943d9771efedee51fefb3191b
Marie-Pierre Oudot <marie-pierre.oudot@cea.fr>
2025-05-19 16:52:20 +0200

    New tests

--- test_link/test_projection.py
+++ test_link/test_projection.py
@@ -94,3 +94,42 @@ def test_projection_arc_circle(capfd):
 
     out, err = capfd.readouterr()
     assert len(err) == 0
+
+def test_projection_glued_curves(capfd):
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+
+    # Création du cylindre creux Vol0000
+    ctx.getGeomManager().newHollowCylinder (Mgx3D.Point(0, 0, 0), .5, 1, Mgx3D.Vector(10, 0, 0), 9.000000e+01, "A")
+    # Copie de  Vol0000
+    ctx.getGeomManager ( ).copy (["Vol0000"], True,"B")
+    # Rotation de Vol0000 suivant  [  [ 0, 0, 0] ,  [ 1, 0, 0] , 90] 
+    ctx.getGeomManager ( ).rotate (["Vol0000"], Mgx3D.RotX(90))
+    # Fusion Booléenne de  Vol0000 Vol0001
+    ctx.getGeomManager ( ).fuse (["Vol0000","Vol0001"])
+    # Fusion de surfaces Surf0000 Surf0006
+    ctx.getGeomManager ( ).joinSurfaces (["Surf0000","Surf0006"])
+    # Fusion de surfaces Surf0001 Surf0007
+    ctx.getGeomManager ( ).joinSurfaces (["Surf0001","Surf0007"])
+    # Fusion de courbes Crb0000 Crb0012
+    ctx.getGeomManager ( ).joinCurves (["Crb0000","Crb0012"])
+    # Création d'un bloc topologique structuré sans projection (Vol0002)
+    ctx.getTopoManager().newFreeTopoOnGeometry ("Vol0002")
+    # Affectation d'une projection vers Crb0024 pour les entités topologiques Som0001 Som0005 Ar0009
+    ctx.getTopoManager ( ).setGeomAssociation (["Som0001","Som0005","Ar0009"], "Crb0024", True)
+    # Création du maillage pour tous les blocs
+    ctx.getMeshManager().newAllBlocksMesh()
+    # Annulation de : Création du maillage pour tous les blocs
+    ctx.undo()
+
+    # Copie de  Crb0024
+    ctx.getGeomManager ( ).copy (["Crb0024"], False,"")
+    # Translation de Crb0025 suivant  [ .5, 0, 0] 
+    ctx.getGeomManager ( ).translate (["Crb0025"], Mgx3D.Vector(.5, 0, 0))
+    # Affectation d'une projection vers Crb0025 pour les entités topologiques Som0001 Som0005 Ar0009
+    ctx.getTopoManager ( ).setGeomAssociation (["Som0001","Som0005","Ar0009"], "Crb0025", True)
+    # Création du maillage pour tous les blocs
+    ctx.getMeshManager().newAllBlocksMesh()
+
+    out, err = capfd.readouterr()
+    assert len(err) == 0
