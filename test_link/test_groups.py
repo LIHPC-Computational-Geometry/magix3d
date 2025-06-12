622405e01411cc94abe996b7db82e7e385f6b5c3
Marie-Pierre Oudot <marie-pierre.oudot@cea.fr>
2025-06-12 16:52:08 +0200

    Add tests for create commands with groups

--- test_link/test_groups.py
+++ test_link/test_groups.py
@@ -12,7 +12,7 @@ def test_group_box():
     coque = "Coque"
     ctx.getTopoManager().newBoxWithTopo (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), 10, 10, 10, acier)
     assert_all(ctx, "Bl0000", [acier])
-
+    
     ctx.getTopoManager().newBoxWithTopo (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), 10, 10, 10)
     assert_all(ctx, "Bl0001", [hors_group])
     ctx.getGeomManager().addToGroup(["Vol0001"], 3, plastique)
@@ -73,3 +73,65 @@ def assert_all(ctx, block, group_names):
         assert set(vol_info.curves()) == set(ctx.getGroupManager().getGeomCurves(group_name, 3))
         # Lien implicite group -> vertices
         assert set(vol_info.vertices()) == set(ctx.getGroupManager().getGeomVertices(group_name, 3))
+
+def test_new_box():
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+    # Création d'une boite avec une topologie
+    ctx.getTopoManager().newBoxWithTopo (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), 10, 10, 10, "aaa")
+    vol_info = ctx.getGeomManager().getInfos("Vol0000",3)
+    # Les entités de dimension inférieure à 3 sont affectées au groupe aaa de manière implicite
+    assert "aaa" in vol_info.groupsName()
+    surf_info = ctx.getGeomManager().getInfos("Surf0000",2)
+    assert "aaa" not in surf_info.groupsName()
+    crb_info = ctx.getGeomManager().getInfos("Crb0000",1)
+    assert "aaa" not in crb_info.groupsName()
+    pt_info = ctx.getGeomManager().getInfos("Pt0000",0)
+    assert "aaa" not in pt_info.groupsName()
+
+def test_section_by_plane():
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+    # Création d'une boite avec une topologie
+    ctx.getTopoManager().newBoxWithTopo (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), 10, 10, 10, "box")
+    # Section par un plan entre géométries avec topologies
+    ctx.getGeomManager ( ).sectionByPlane (["Vol0000"], Mgx3D.Plane(Mgx3D.Point(0, 0, .5), Mgx3D.Vector(0, 0, 1)), "aaa")
+    # Les entités de dimension inférieure à 2 sont affectées au groupe aaa de manière explicite
+    surf_info = ctx.getGeomManager().getInfos("Surf0010",2)
+    assert "aaa" in surf_info.groupsName()
+    crb_info = ctx.getGeomManager().getInfos("Crb0014",1)
+    assert "aaa" in crb_info.groupsName()
+    pt_info = ctx.getGeomManager().getInfos("Pt0008",0)
+    assert "aaa" in pt_info.groupsName()
+
+def test_new_vertices_curves_and_planar_surface():
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+    # Création d'une boite avec une topologie
+    ctx.getTopoManager().newBoxWithTopo (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), 10, 10, 10, "box")
+    # Création d'une surface avec vecteur de points
+    ctx.getGeomManager ( ).newVerticesCurvesAndPlanarSurface ([Mgx3D.Point(1, 1, 0),Mgx3D.Point(1, 1, 1),Mgx3D.Point(1, 0, 1),Mgx3D.Point(1, 0, 0)], "aaa")
+    # Les entités de dimension inférieure à 2 sont affectées au groupe aaa de manière explicite
+    # Cette commande crée 4 points + 4 courbes + 1 surface
+    surf_info = ctx.getGeomManager().getInfos("Surf0006",2)
+    assert "aaa" in surf_info.groupsName()
+    crb_info = ctx.getGeomManager().getInfos("Crb0012",1)
+    assert "aaa" in crb_info.groupsName()
+    pt_info = ctx.getGeomManager().getInfos("Pt0008",0)
+    assert "aaa" in pt_info.groupsName()
+
+def test_new_planar_surface():
+    ctx = Mgx3D.getStdContext()
+    ctx.clearSession() # Clean the session after the previous test
+    # Création d'une boite avec une topologie
+    ctx.getTopoManager().newBoxWithTopo (Mgx3D.Point(0, 0, 0), Mgx3D.Point(1, 1, 1), 10, 10, 10, "box")
+    # Création de la surface Surf0006
+    ctx.getGeomManager ( ).newPlanarSurface (["Crb0002","Crb0006","Crb0010","Crb0011"], "aaa")
+    # Les entités de dimension inférieure à 2 sont affectées au groupe aaa de manière implicite
+    # Cette commande crée 1 surface à partir de 4 courbes existantes
+    surf_info = ctx.getGeomManager().getInfos("Surf0006",2)
+    assert "aaa" in surf_info.groupsName()
+    crb_info = ctx.getGeomManager().getInfos("Crb0002",1)
+    assert "aaa" not in crb_info.groupsName()
+    pt_info = ctx.getGeomManager().getInfos("Pt0002",0)
+    assert "aaa" not in pt_info.groupsName()
