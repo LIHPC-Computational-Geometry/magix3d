c09d6105714a623e36d6d3e865fb6e7550fe13b3
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2025-10-17 09:21:27 +0200

    Template group entity

--- src/Core/protected/Group/Group2D.h
+++ src/Core/protected/Group/Group2D.h
@@ -1,11 +1,4 @@
 /*----------------------------------------------------------------------------*/
-/** \file Group2D.h
- *
- *  \author Eric Brière de l'Isle
- *
- *  \date 24/10/2012
- */
-/*----------------------------------------------------------------------------*/
 #ifndef MGX3D_GROUP_GROUP2D_H_
 #define MGX3D_GROUP_GROUP2D_H_
 /*----------------------------------------------------------------------------*/
@@ -13,16 +6,6 @@
 #include <TkUtil/UTF8String.h>
 /*----------------------------------------------------------------------------*/
 namespace Mgx3D {
-
-namespace Geom {
-class Surface;
-}
-namespace Topo {
-class CoFace;
-}
-namespace Mesh {
-class Surface;
-}
 
 namespace Internal {
 class Context;
@@ -69,52 +52,6 @@ public:
     virtual Utils::Entity::objectType getType() const {return Utils::Entity::Group2D;}
 
     /*------------------------------------------------------------------------*/
-    /// pour l'affichage d'informations
-    friend TkUtil::UTF8String & operator << (TkUtil::UTF8String & , const Group2D &);
-    friend std::ostream & operator << (std::ostream & , const Group2D &);
-
-    /// retourne les infos sur l'entité
-    virtual std::string getInfos();
-
-     /*------------------------------------------------------------------------*/
-	/// retourne les entités du groupe.
-     virtual std::vector<Utils::Entity*> getEntities() const;
-
-    /*------------------------------------------------------------------------*/
-    /// accesseur sur la liste des Surfaces référencées
-    std::vector<Geom::Surface*>& getSurfaces() {return m_surfaces;}
-    const std::vector<Geom::Surface*>& getSurfaces() const {return m_surfaces;}
-
-    /// accesseur sur la liste des faces topologiques
-    std::vector<Topo::CoFace*>& getCoFaces() {return m_cofaces;}
-    const std::vector<Topo::CoFace*>& getCoFaces() const {return m_cofaces;}
-
-    std::vector<Mesh::Surface*>& getMeshSurfaces() {return m_mesh_surfaces;}
-    const std::vector<Mesh::Surface*>& getMeshSurfaces() const {return m_mesh_surfaces;}
-
-    virtual bool empty() const {return m_surfaces.empty() && m_cofaces.empty() && m_mesh_surfaces.empty();}
-
-    /*------------------------------------------------------------------------*/
-    /// Enlève une Surface du groupe
-    void remove(Geom::Surface* surf, const bool exceptionIfNotFound=true);
-    void remove(Topo::CoFace* coface, const bool exceptionIfNotFound=true);
-    void remove(Mesh::Surface* surf, const bool exceptionIfNotFound=true);
-
-    /*------------------------------------------------------------------------*/
-    /// Ajoute une Surface au groupe
-    void add(Geom::Surface* surf);
-    void add(Topo::CoFace* coface);
-    void add(Mesh::Surface* surf);
-
-    /*------------------------------------------------------------------------*/
-    /// Recherche une Surface dans le groupe
-    bool find(Geom::Surface* surf);
-    /// Recherche une face dans le groupe
-    bool find(Topo::CoFace* coface);
-    /// Recherche une Surface dans le groupe
-    bool find(Mesh::Surface* surf);
-
-    /*------------------------------------------------------------------------*/
     /** \brief  Fournit une représentation textuelle de l'entité.
 	 * \param	true si l'entité fourni la totalité de sa description, false si
 	 * 			elle ne fournit que les informations non calculées (objectif :
@@ -125,17 +62,6 @@ public:
     virtual Mgx3D::Utils::SerializedRepresentation* getDescription (
 													bool alsoComputed) const;
 #endif
-
-
-private:
-    /// les Surfaces référencées par ce groupe
-    std::vector<Geom::Surface*> m_surfaces;
-
-    /// les faces référencées par ce groupe
-    std::vector<Topo::CoFace*> m_cofaces;
-
-    /// les surfaces de maillage référencés par ce groupe
-    std::vector<Mesh::Surface*> m_mesh_surfaces;
 };
 /*----------------------------------------------------------------------------*/
 } // end namespace Group
