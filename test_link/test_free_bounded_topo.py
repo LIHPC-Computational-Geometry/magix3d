6d97bc40084e4c6f93ce254f07848d41b4c0e4c4
Marie-Pierre Oudot <marie-pierre.oudot@cea.fr>
2026-01-19 11:06:00 +0100

    Upgrade tests : closer BoundingBox

--- test_link/test_free_bounded_topo.py
+++ test_link/test_free_bounded_topo.py
@@ -11,6 +11,6 @@ def test_freeboundedtopo():
     gm.newVertex(Mgx3D.Point(-.7, .45, -.1))
     tm.newFreeBoundedTopoInGroup ("aaa", 3, ["Pt0000","Pt0001"])
     c = tm.getCoord("Som0007")
-    assert c.getX() == -0.6999999
-    assert c.getY() ==  0.4500001
-    assert c.getZ() ==  1.2000001
+    assert c.getX() == -0.7
+    assert c.getY() ==  0.45
+    assert c.getZ() ==  1.2
