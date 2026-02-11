24c2069e3c0cab7c296d863b3f4dc0f3ab35c919
Claire Roche <claireroche47@gmail.com>
2026-02-11 09:16:21 +0100

    issue_249: Fix python writing of BSpline

--- src/Core/Geom/GeomManager.cpp
+++ src/Core/Geom/GeomManager.cpp
@@ -2371,8 +2371,10 @@ newBSpline(Vertex* vtx1,
             new CommandNewBSpline(getContext(), vtx1, vp, vtx2, deg_min, deg_max, groupName);
     // trace dans le script
     TkUtil::UTF8String cmd (TkUtil::Charset::UTF_8);
-    cmd << getContextAlias ( ) << ".getGeomManager ( ).newBSpline (" << Internal::scriptablesToPythonList<Point> (vp) << ", ";
-    cmd <<vtx2->getName()<<"\","<<(short)deg_min<<", "<<(short)deg_max
+    cmd << getContextAlias ( ) << ".getGeomManager ( ).newBSpline (" ;
+    cmd << "\"" << vtx1->getName() << "\"," ;
+    cmd << Internal::scriptablesToPythonList<Point> (vp) << ", ";
+    cmd << "\"" << vtx2->getName()<<"\","<<(short)deg_min<<", "<<(short)deg_max
     		<<", \""<<groupName<<"\")";
     command->setScriptCommand(cmd);
 
