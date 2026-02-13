82d2db0010bc50f0438868bb3049864c806ba835
calderans <calderan.simon@gmail.com>
2026-02-13 16:57:13 +0100

    - first implementation of CommandAero

--- src/Core/protected/Topo/CommandAero.h
+++ src/Core/protected/Topo/CommandAero.h
@@ -1,17 +1,17 @@
 #ifndef MAGIX3D_COMMANDAERO_H
 #define MAGIX3D_COMMANDAERO_H
 /*----------------------------------------------------------------------------*/
-#include "Topo/CommandEditTopo.h"
+#include "Topo/CommandCreateTopo.h"
 
 /*----------------------------------------------------------------------------*/
 namespace Mgx3D {
 /*----------------------------------------------------------------------------*/
 namespace Topo {
 /*----------------------------------------------------------------------------*/
-class CommandAero: public CommandEditTopo{
+class CommandAero: public CommandCreateTopo{
 
 public:
-    CommandAero(Internal::Context& c);
+    CommandAero(Internal::Context& c, std::string filename);
     virtual ~CommandAero();
 
     /*------------------------------------------------------------------------*/
@@ -22,7 +22,15 @@ public:
     void getPreviewRepresentation(Utils::DisplayRepresentation &dr);
 
     private:
+    Internal::Context& m_c;
 
+    std::string m_filename;
+
+
+    std::map<int, Topo::Vertex*> m_gmdsNode2TopoNode;
+    std::map<int, Topo::CoEdge*> m_gmdsEdge2TopoEdge;
+    std::map<int, Topo::CoFace*> m_gmdsFace2TopoFace;
+    std::map<int, Topo::Block*> m_gmdsBlock2TopoBlock;
 };
 }
 }
