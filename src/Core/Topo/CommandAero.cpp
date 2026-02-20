efb754c594f3a8a80e8d663816dc63e318eacb90
calderans <calderan.simon@gmail.com>
2026-02-20 13:35:59 +0100

    - CommandAero register topo to manager

--- src/Core/Topo/CommandAero.cpp
+++ src/Core/Topo/CommandAero.cpp
@@ -27,24 +27,24 @@ CommandAero::~CommandAero() {
 void CommandAero::internalExecute() {
     int pos = m_filename.find_last_of('/');
     std::string dir = m_filename.substr(0, pos+1);
-    //std::string filename = m_filename.substr(pos + 1);
-    gmds::AeroPipeline_3D pipeline(m_filename,dir);
+    gmds::AeroPipeline_3D* pipeline = new gmds::AeroPipeline_3D(m_filename,dir);
 
-    pipeline.execute();
-    gmds::Blocking3D* blocking(pipeline.getBlocking());
+    pipeline->execute();
+    gmds::Blocking3D *blocking = pipeline->getBlocking();
 
     std::cout<<"nb nodes "<<blocking->getNbNodes()<<std::endl;
-
     for (auto n : blocking->nodes()) {
         gmds::Node node = blocking->get<gmds::Node>(n);
 
-        Point p(node.X(), node.Y(), node.Z());
+        if (!node.getIDs<gmds::Region>().empty()) {
+            Point p(node.X(), node.Y(), node.Z());
 
-        Vertex *vtx = new Vertex(m_c, p);
+            Vertex *vtx = new Vertex(m_c, p);
 
-        m_gmdsNode2TopoNode[n] = vtx;
-        getInfoCommand().addTopoInfoEntity(vtx, Internal::InfoCommand::CREATED);
-        m_group_helper.addToGroup("Aero_0D", vtx);
+            m_gmdsNode2TopoNode[n] = vtx;
+            getInfoCommand().addTopoInfoEntity(vtx, Internal::InfoCommand::CREATED);
+            m_group_helper.addToGroup("Aero_0D", vtx);
+        }
     }
     std::cout<<"nb edges "<<blocking->getNbEdges()<<std::endl;
     for (auto e : blocking->edges()) {
@@ -73,49 +73,74 @@ void CommandAero::internalExecute() {
 
         std::vector<Topo::Edge*> edges;
         Edge* edge0 = new Edge(m_c, m_gmdsEdge2TopoEdge[e0]);
+            getInfoCommand().addTopoInfoEntity(edge0, Internal::InfoCommand::CREATED);
         Edge* edge1 = new Edge(m_c, m_gmdsEdge2TopoEdge[e1]);
+            getInfoCommand().addTopoInfoEntity(edge1, Internal::InfoCommand::CREATED);
         Edge* edge2 = new Edge(m_c, m_gmdsEdge2TopoEdge[e2]);
+            getInfoCommand().addTopoInfoEntity(edge2, Internal::InfoCommand::CREATED);
         Edge* edge3 = new Edge(m_c, m_gmdsEdge2TopoEdge[e3]);
+            getInfoCommand().addTopoInfoEntity(edge3, Internal::InfoCommand::CREATED);
         edges.push_back(edge0);
         edges.push_back(edge1);
         edges.push_back(edge2);
         edges.push_back(edge3);
 
         CoFace *coface = new CoFace(m_c, edges, true);
-        m_gmdsFace2TopoFace[f] = coface;
         getInfoCommand().addTopoInfoEntity(coface, Internal::InfoCommand::CREATED);
         m_group_helper.addToGroup("Aero_2D", coface);
+        m_gmdsFace2TopoFace[f] = coface;
     }
     std::cout<<"nb blocks "<<blocking->getNbRegions()<<std::endl;
     for (auto r : blocking->regions()) {
         gmds::Region region = blocking->get<gmds::Region>(r);
-        std::vector<gmds::TCellID> nodesID = region.getIDs<gmds::Node>();
+        std::vector<gmds::TCellID> gnodesID = region.getIDs<gmds::Node>();
+        std::vector<gmds::TCellID> nodesID;
+        nodesID.resize(8);
+        nodesID[0] = region.getIDs<gmds::Node>()[0];
+        nodesID[1] = region.getIDs<gmds::Node>()[3];
+        nodesID[2] = region.getIDs<gmds::Node>()[1];
+        nodesID[3] = region.getIDs<gmds::Node>()[2];
+        nodesID[4] = region.getIDs<gmds::Node>()[4];
+        nodesID[5] = region.getIDs<gmds::Node>()[7];
+        nodesID[6] = region.getIDs<gmds::Node>()[5];
+        nodesID[7] = region.getIDs<gmds::Node>()[6];
+
+
         std::vector<gmds::TCellID> facesID;
-        facesID.reserve(6);
+        facesID.resize(6);
         //On reordonne les faces gmds pour correspondre aux faces mgx
-        facesID[0] = region.getIDs<gmds::Face>()[0];
-        facesID[1] = region.getIDs<gmds::Face>()[5];
-        facesID[2] = region.getIDs<gmds::Face>()[1];
-        facesID[3] = region.getIDs<gmds::Face>()[3];
-        facesID[4] = region.getIDs<gmds::Face>()[2];
-        facesID[5] = region.getIDs<gmds::Face>()[4];
+        facesID[0] = region.getIDs<gmds::Face>()[2];
+        facesID[1] = region.getIDs<gmds::Face>()[3];
+        facesID[2] = region.getIDs<gmds::Face>()[4];
+        facesID[3] = region.getIDs<gmds::Face>()[5];
+        facesID[4] = region.getIDs<gmds::Face>()[0];
+        facesID[5] = region.getIDs<gmds::Face>()[1];
 
         std::vector<Topo::Vertex*> vertices;
+        std::vector<std::string> nvertices;
+        std::vector<std::string> gvertices;
+
         for (auto n : nodesID) {
             vertices.push_back(m_gmdsNode2TopoNode[n]);
         }
+
         std::vector<Topo::Face*> faces;
         for (auto f : facesID) {
-            Face* face = new Face(m_c,m_gmdsFace2TopoFace[f]);
-            faces.push_back(face);
+            Face* topoface = new Face(m_c,m_gmdsFace2TopoFace[f]);
+            getInfoCommand().addTopoInfoEntity(topoface, Internal::InfoCommand::CREATED);
+            faces.push_back(topoface);
         }
 
         Block* block = new Block(m_c, faces, vertices, true);
+        block->structure(&getInfoCommand());
         m_gmdsBlock2TopoBlock[r] = block;
         getInfoCommand().addTopoInfoEntity(block, Internal::InfoCommand::CREATED);
         m_group_helper.addToGroup("Aero_3D", block);
     }
+    free(pipeline);
 
+    // enregistrement des nouvelles entités dans le TopoManager
+    registerToManagerCreatedEntities();
 }
 
 void CommandAero::getPreviewRepresentation(Utils::DisplayRepresentation &dr) {
