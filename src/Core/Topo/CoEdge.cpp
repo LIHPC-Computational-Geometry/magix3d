4949a3fd5cd65dc6b974fba77d1d4f21f42d9f18
Claire Roche <claireroche47@gmail.com>
2026-03-13 08:36:06 +0100

    issue-121: decomment code

--- src/Core/Topo/CoEdge.cpp
+++ src/Core/Topo/CoEdge.cpp
@@ -1426,11 +1426,11 @@ getPoints(CoEdgeMeshingProperty *prop, std::vector<Utils::Math::Point> &points, 
                 else
                 {
                     // TODO [EB]: il faut calculer la longueur de l'arête projetée sur la courbe
-                    double l = Geom::OCCHelper::curvilinearDistance(curve->getOCCEdges(), getVertices()[0]->getCoord(), getVertices()[1]->getCoord());
-                    prop->initCoeff(l);
-                    std::cout << "len: " << l << std::endl;
+                    //double l = Geom::OCCHelper::curvilinearDistance(curve->getOCCEdges(), getVertices()[0]->getCoord(), getVertices()[1]->getCoord());
+                    //prop->initCoeff(l);
+                    //std::cout << "len: " << l << std::endl;
 
-                    /*
+
                     // regarde si l'on est dans le cas de l'arête projetée sur l'intégralité de la courbe
                     auto crv_vertices = curve->getVertices();
                     auto vertices2 = getVertices();
@@ -1463,7 +1463,7 @@ getPoints(CoEdgeMeshingProperty *prop, std::vector<Utils::Math::Point> &points, 
                         double dist = vertices2[0]->getCoord().length(vertices2[1]->getCoord());
                         prop->initCoeff(dist);
                     }
-                    */
+
                 } // end else / if (curveToBeDeleted)
             }
             catch (TkUtil::Exception &exc)
