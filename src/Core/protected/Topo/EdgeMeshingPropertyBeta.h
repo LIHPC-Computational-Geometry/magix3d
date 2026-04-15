cbde00bcae4d5f8a4c3e8a82592877d4254c98a5
Claire Roche <claireroche47@gmail.com>
2026-04-15 14:56:55 +0200

    issue-273: fix bug beta law indirect direction
    issue-273: code formatting
    
    issue-273: clang warnings cleaning
    
    issue-273: fix beta parameter computation for indirect topo edge discretization
    
    issue-273: update python unit test

--- src/Core/protected/Topo/EdgeMeshingPropertyBeta.h
+++ src/Core/protected/Topo/EdgeMeshingPropertyBeta.h
@@ -1,14 +1,4 @@
 /*----------------------------------------------------------------------------*/
-/*
- * \file EdgeMeshingPropertyBeta.h
- *
- *  \author Eric Brière de l'Isle
- *
- *  \date 21/1/2015
- *
- *  d'après ce qui a été fourni par Thierry Hocquellet et nommé "loi beta de resserrement sur paroi"
- */
-/*----------------------------------------------------------------------------*/
 #ifndef EDGEPROPERTYMESHINGBETA_H_
 #define EDGEPROPERTYMESHINGBETA_H_
 /*----------------------------------------------------------------------------*/
@@ -22,32 +12,37 @@ namespace Topo {
    @brief Propriété de la discrétisation d'une arête avec une discrétisation
    ayant un resserrement sur paroi
  */
-class EdgeMeshingPropertyBeta : public CoEdgeMeshingProperty {
+class EdgeMeshingPropertyBeta : public CoEdgeMeshingProperty
+{
 public:
 
     /*------------------------------------------------------------------------*/
-    /** Constructeur avec un nombre de bras spécifié,
+    /** @brief Constructeur avec un nombre de bras spécifié,
      *  la valeur de beta
      *  et la longueur du premier bras
-     *  \param beta coeff de resserrement , beta=1+epsilon  ; epsilon>  0;
+     *  @param nbBras nombre de bras
+     *  @param beta coeff de resserrement , beta=1+epsilon  ; epsilon>  0;
      *  en pratique beta peut varier de 1.01 (peu resséré) à 1.00001 (très resséré)
      *  Si la longueur du premier bras est non nulle, alors beta est calculé
+     *  @param isDirect sens de la discrétisation sur l'arête topo
+     *  @param initWithFirstEdge true si on impose la taille de première maille
+     *  @param meshingEdgeLength taille de la première maille imposée
      */
-    EdgeMeshingPropertyBeta(int nb, double beta, bool isDirect=true, bool initWithFirstEdge=false, double meshingEdgeLength=0.0);
+    EdgeMeshingPropertyBeta(int nbBras, double beta, bool isDirect=true, bool initWithFirstEdge=false, double meshingEdgeLength=0.0);
 
     /// Pour les pythoneries, cast par opérateur = :
-    EdgeMeshingPropertyBeta (const CoEdgeMeshingProperty&);
+    explicit EdgeMeshingPropertyBeta (const CoEdgeMeshingProperty&);
 
     /*------------------------------------------------------------------------*/
     /// Comparaison de 2 propriétés.
-    virtual bool operator == (const CoEdgeMeshingProperty&) const;
+    bool operator == (const CoEdgeMeshingProperty&) const override;
 
     /*------------------------------------------------------------------------*/
     /// Accesseur sur le nom de la méthode de maillage
-    virtual std::string getMeshLawName() const {return "Beta";}
+    std::string getMeshLawName() const override {return "Beta";}
 
     /// change le nombre de bras demandé pour un côté
-    virtual void setNbEdges(const int nb);
+    void setNbEdges(int nb) override;
 
     /// le ratio entre deux bras successifs
     double getBeta() const {return m_beta;}
@@ -58,28 +53,28 @@ public:
     double getFirstEdgeLength() const {return m_arm1;}
     void setFirstEdgeLength(const double & arm1) {m_arm1 = arm1;}
 
-    #ifndef SWIG
+#ifndef SWIG
     /** Création d'un clone, on copie toutes les informations */
-    virtual CoEdgeMeshingProperty* clone() {return new  EdgeMeshingPropertyBeta(*this);}
+    CoEdgeMeshingProperty* clone() override {return new  EdgeMeshingPropertyBeta(*this);}
 
-    // initialisation avant appel à nextCoeff
-    virtual void initCoeff();
+    /// initialisation avant appel à nextCoeff
+    void initCoeff() override;
 
     /// initialisation avant appel à nextCoeff pour le cas où la longueur est utile
-    virtual void initCoeff(double length);
+    void initCoeff(double length) override;
 
     /// retourne le coefficient suivant pour les noeuds internes
-    virtual double nextCoeff();
+    double nextCoeff() override;
 
     /*------------------------------------------------------------------------*/
     /// Script pour la commande de création Python
-    virtual TkUtil::UTF8String getScriptCommand() const;
+    TkUtil::UTF8String getScriptCommand() const override;
 
     /*------------------------------------------------------------------------*/
     /** Ajoute les informations spécifiques de cette discrétisation à
      *  la représentation textuelle
      */
-    virtual void addDescription(Utils::SerializedRepresentation& topoProprietes) const;
+    void addDescription(Utils::SerializedRepresentation& topoProprietes) const override;
 
 #endif
 
@@ -89,10 +84,10 @@ private:
      feta=1-->  frontière amont)
      transformation de (0,1) sur (0,1) permettant de resserrer le maillage au voisinage du corps
      */
-    double resserre(double eta, double beta);
+    static double resserre(double eta, double beta);
 
     /// calcul de beta
-    double computeBeta(const double lg);
+    double computeBeta(double lg) const;
 
     /// valeur de beta pour le resserrement (de la forme 1+epsilon)
     double m_beta;
